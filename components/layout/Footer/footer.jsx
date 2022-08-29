8c4713de87020260b08a6eee0f59f55d9a6d7864
grzeskid <szymanekgrzegorz@gmail.com>
2022-08-29 22:21:49 +0200

    footer structure

--- components/layout/Footer/footer.jsx
+++ components/layout/Footer/footer.jsx
@@ -1,8 +1,29 @@
+import { Logo } from "../Navbar/Logo";
+
 const Footer = () => {
     return (
-        <div>
-           <h1>tu będzie footer</h1>
-        </div>
+        <footer>
+            <div className="container">
+
+                <article className="footer-content">
+                    
+                    <div className="footer-logo">
+                        <Logo></Logo>
+                    </div>
+                    
+                    <div className="footer-socials">
+                        <i className="fa-brands fa-square-facebook"></i>
+                        <i className="fa-brands fa-instagram"></i>
+                    </div>
+                    
+                </article>
+
+                <article className="footer-copyright">
+                    <span>COPYRIGHT &copy; JUNIORCODERS 2022</span>
+                </article>
+
+            </div>
+        </footer>
     );
 };
 
