f5280cc6ab3deb8b9e2b9fe290394cd5be0af813
PawelPazderski <pawel.pazderski@op.pl>
2022-09-05 13:04:35 +0200

    contact page background, title

--- pages/contact/index.jsx
+++ pages/contact/index.jsx
@@ -1,5 +1,14 @@
+import { useState, useRef} from 'react';
+
 const Contact = () => {
-  return <div>tu będzie kontakt</div>;
+
+  return (
+    <>
+      <div className="contact-container">
+        <h2 className="contact-title">Napisz do nas</h2>
+      </div>
+    </>
+  )
 };
 
 export default Contact;
