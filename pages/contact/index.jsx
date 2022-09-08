5a15e07ac1991e1396f5e6f3dc42d9fa7661e53f
Kamil Kiliasiński <kamilkiliasinski@gmail.com>
2022-09-08 19:09:43 +0200

    fix small buggs in contact

--- pages/contact/index.jsx
+++ pages/contact/index.jsx
@@ -1,147 +1,159 @@
-import { useState, useRef} from 'react';
-import emailjs from '@emailjs/browser';
-import {SERVICE_ID, TEMPLATE_ID, PUBLIC_KEY} from '../../emailkey';
-// import {ContactModal} from '../../components/ContactModal'
-// import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
-// import 'bootstrap/dist/css/bootstrap.min.css';
-import Swal from 'sweetalert2';
+import { useState, useRef } from "react";
+import emailjs from "@emailjs/browser";
+import Swal from "sweetalert2";
 
 const Contact = () => {
-  const [msgFrom, setMsgFrom] = useState("")
-  const [msgFromErr, setMsgFromErr] = useState(false)
-  const [email, setEmail] = useState("")
-  const [emailErr, setEmailErr] = useState(false)
-  const [msg, setMsg] = useState("")
-  const [msgErr, setMsgErr] = useState(false)
-  const [agreed, setAgreed] = useState(false)
-  const [agreeErr, setAgreeErr] = useState(false)
+  const [msgFrom, setMsgFrom] = useState("");
+  const [msgFromErr, setMsgFromErr] = useState(false);
+  const [email, setEmail] = useState("");
+  const [emailErr, setEmailErr] = useState(false);
+  const [msg, setMsg] = useState("");
+  const [msgErr, setMsgErr] = useState(false);
+  const [agreed, setAgreed] = useState(false);
+  const [agreeErr, setAgreeErr] = useState(false);
 
-  const form = useRef()
-  const checkmark = useRef()
+  const form = useRef();
+  const checkmark = useRef();
 
   const fireModal = () => {
     Swal.fire({
-      title: 'Wiadomość wysłana',
-      text: 'Dziękujemy, skontaktujemy się z Tobą jak najszybciej',
+      title: "Wiadomość wysłana",
+      text: "Dziękujemy, skontaktujemy się z Tobą jak najszybciej",
       background: "#FFD100",
       timer: 2000,
       showConfirmButton: false,
       timerProgressBar: true,
       customClass: {
-        popup: 'swal-appearance'
-      }
-    })
-  }
+        popup: "swal-appearance",
+      },
+    });
+  };
 
   const preventEnter = (e) => {
-    if(e.key === 'Enter'){
-        e.preventDefault()
+    if (e.key === "Enter") {
+      e.preventDefault();
     }
-  }
+  };
 
   const handleChangeName = (e) => {
     const newValueIsValid = !e.target.validity.patternMismatch;
 
     if (msgFromErr && newValueIsValid) {
-        setMsgFromErr(false);
+      setMsgFromErr(false);
     }
-    setMsgFrom(e.target.value)
-  }
+    setMsgFrom(e.target.value);
+  };
 
   const validateMsgFrom = (e) => {
     if (e.target.validity.patternMismatch) {
-        setMsgFromErr(true)
+      setMsgFromErr(true);
     } else {
-        setMsgFromErr(false)
-        }
-  }
+      setMsgFromErr(false);
+    }
+  };
 
   const handleChangeEmail = (e) => {
     const newValueIsValid = !e.target.validity.patternMismatch;
 
     if (emailErr && newValueIsValid) {
-        setEmailErr(false);
+      setEmailErr(false);
     }
-    setEmail(e.target.value)
-  }
+    setEmail(e.target.value);
+  };
 
   const validateEmail = (e) => {
     if (e.target.validity.patternMismatch) {
-        setEmailErr(true)
+      setEmailErr(true);
     } else {
-            setEmailErr(false)
-            }
-  }
+      setEmailErr(false);
+    }
+  };
 
   const handleChangeMsg = (e) => {
     if (e.target.value.length >= 10) {
-        setMsgErr(false);
+      setMsgErr(false);
     }
-    setMsg(e.target.value)
-  }
+    setMsg(e.target.value);
+  };
 
   const validateMsg = (e) => {
     if (e.target.value.length >= 10 || e.target.value.length == 0) {
-        setMsgErr(false)
+      setMsgErr(false);
     } else {
-            setMsgErr(true)
-            }
+      setMsgErr(true);
     }
+  };
 
-    const handleCheckAgree = (e) => {
-      if (e.target.checked) {
-          setAgreed(true)
-          setAgreeErr(false)
-      } else {
-          setAgreed(false)
-      }
-  }
+  const handleCheckAgree = (e) => {
+    if (e.target.checked) {
+      setAgreed(true);
+      setAgreeErr(false);
+    } else {
+      setAgreed(false);
+    }
+  };
 
   const sendEmail = (e) => {
     e.preventDefault();
 
-    if(!msgFrom.length) {
-      setMsgFromErr(true)
+    if (!msgFrom.length) {
+      setMsgFromErr(true);
     }
 
-    if(!email.length) {
-        setEmailErr(true)
+    if (!email.length) {
+      setEmailErr(true);
     }
 
-    if(!msg.length) {
-        setMsgErr(true)
+    if (!msg.length) {
+      setMsgErr(true);
     }
 
     if (!agreed) {
-        setAgreeErr(true)
+      setAgreeErr(true);
     }
 
-    if (!msgFromErr && !emailErr && !msgErr && !agreeErr && msgFrom.length && email.length && msg.length && agreed) {
-      emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, form.current, PUBLIC_KEY)
-        .then((result) => {
+    if (
+      !msgFromErr &&
+      !emailErr &&
+      !msgErr &&
+      !agreeErr &&
+      msgFrom.length &&
+      email.length &&
+      msg.length &&
+      agreed
+    ) {
+      emailjs
+        .sendForm(
+          process.env.SERVICE_ID,
+          process.env.TEMPLATE_ID,
+          form.current,
+          process.env.PUBLIC_KEY
+        )
+        .then(
+          (result) => {
             console.log(result.text, "message sent");
-  
-        }, (error) => {
+          },
+          (error) => {
             console.log(error.text);
-            alert(error.text)
-            return
-        });
+            alert(error.text);
+          }
+        );
 
-        clearForm()
-        fireModal()
+      clearForm();
+      fireModal();
     }
-  }
+  };
 
   const clearForm = () => {
-    setMsgFrom("")
-    setEmail("")
-    setMsg("")
-    setAgreed(false)
-    checkmark.current.checked = false
-}
+    setMsgFrom("");
+    setEmail("");
+    setMsg("");
+    setAgreed(false);
+    checkmark.current.checked = false;
+  };
 
   return (
-    <>
+    <div className="contact">
       <div className="contact-container">
         <form ref={form} className="contact-form" onSubmit={sendEmail}>
           <h2 className="contact-form-title">Napisz do nas</h2>
@@ -150,88 +162,82 @@ const Contact = () => {
             placeholder="Imię"
             type="text"
             value={msgFrom}
-            className={ msgFromErr ? "form-control-alert" : null}
+            className={msgFromErr ? "form-control-alert" : null}
             onChange={handleChangeName}
             pattern="^\S{2,}$"
             onBlur={validateMsgFrom}
             onKeyPress={preventEnter}
           />
-          <h6  
-            className={ 
-            msgFromErr 
-            ? 
-            "contact-form-alert" 
-            : 
-            "contact-form-alert d-none"
-            }>Imię musi składać się z conajmniej 2 znaków!
+          <h6
+            className={
+              msgFromErr ? "contact-form-alert" : "contact-form-alert d-none"
+            }
+          >
+            Imię musi składać się z conajmniej 2 znaków!
           </h6>
-          <input 
+          <input
             name="user_email"
             placeholder="Email"
             type="text"
             value={email}
-            className={ emailErr ? "form-control-alert" : null}
+            className={emailErr ? "form-control-alert" : null}
             onChange={handleChangeEmail}
             onBlur={validateEmail}
             onKeyPress={preventEnter}
             pattern='^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-z\-0-9]+\.)+[a-z]{2,}))$'
           />
-          <h6  
-            className={ 
-            emailErr 
-            ? 
-            "contact-form-alert" 
-            : 
-            "contact-form-alert d-none"
-            }>Podany email jest niepoprawny!
+          <h6
+            className={
+              emailErr ? "contact-form-alert" : "contact-form-alert d-none"
+            }
+          >
+            Podany email jest niepoprawny!
           </h6>
           <textarea
             name="message"
             placeholder="Twoja wiadomość"
             value={msg}
-            rows= "6"
-            className={ msgErr ? "form-control-alert" : null }
+            rows="6"
+            className={msgErr ? "form-control-alert" : null}
             onChange={handleChangeMsg}
             minLength="20"
             onBlur={validateMsg}
             onKeyPress={preventEnter}
-            style={{resize: "none"}}
+            style={{ resize: "none" }}
           />
-          <h6  
-            className={ 
-            msgErr 
-            ? 
-            "contact-form-alert" 
-            : 
-            "contact-form-alert d-none"
-            }>Wiadomość musi składać się z conajmniej 10 znaków!
+          <h6
+            className={
+              msgErr ? "contact-form-alert" : "contact-form-alert d-none"
+            }
+          >
+            Wiadomość musi składać się z conajmniej 10 znaków!
           </h6>
-          <div className='agree-box'>
+          <div className="agree-box">
             <input
               ref={checkmark}
               type="checkbox"
               id="agreement-input"
               onChange={handleCheckAgree}
-            >
-            </input>
+            ></input>
             <span className="checkmark"></span>
-            <label>Wyrażam zgodę na przetwarzanie danych wpisanych w formularzu w celu udzielenia odpowiedzi na przesłane zapytanie.</label>
+            <label>
+              Wyrażam zgodę na przetwarzanie danych wpisanych w formularzu w
+              celu udzielenia odpowiedzi na przesłane zapytanie.
+            </label>
           </div>
-          <h6  
-            className={ 
-            agreeErr 
-            ? 
-            "contact-form-alert" 
-            : 
-            "contact-form-alert d-none"
-            }>Aby wysłać zapytanie potrzebna jest Twoja zgoda na przetwarzanie danych!
+          <h6
+            className={
+              agreeErr ? "contact-form-alert" : "contact-form-alert d-none"
+            }
+          >
+            Aby wysłać zapytanie potrzebna jest Twoja zgoda na przetwarzanie
+            danych!
           </h6>
           <button type="submit">Wyślij</button>
         </form>
-        <button onClick={fireModal}>modal</button>
       </div>
-    </>
-  )
+    </div>
+  );
 };
 
 export default Contact;
