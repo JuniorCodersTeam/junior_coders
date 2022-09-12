7da999bb011a0fcc87b28589ff9b9cc20e946398
PawelPazderski <pawel.pazderski@op.pl>
2022-09-12 14:40:20 +0200

    title mixin, basic container for projects section

--- pages/projects/index.jsx
+++ pages/projects/index.jsx
@@ -1,7 +1,27 @@
-const Projects = () => {
+import { createClient } from "contentful";
+
+export async function getStaticProps() {
+    const client = createClient({
+        space: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_KEY,
+    });
+
+    const { items: projects } = await client.getEntries({
+        content_type: "projects",
+    });
+
+    return {
+        props: { projects }
+        };
+    }
+
+const Projects = ({ projects }) => {
+
+    console.log(projects)
+
     return (
-        <div>
-            <h1>tu będzie sekcja projekty</h1>
+        <div className="projects-container">
+            <h2 className="projects-title">Nasze projekty</h2>
         </div>
     );
 };
