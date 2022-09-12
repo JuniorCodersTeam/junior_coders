2d93e8fc269f73bbfa3f4fd8221bfd52dbbd69cd
grzeskid <szymanekgrzegorz@gmail.com>
2022-09-12 23:01:34 +0200

    displaying and styling - img to fix

--- components/layout/AboutUs/AboutUs.jsx
+++ components/layout/AboutUs/AboutUs.jsx
@@ -10,21 +10,29 @@ console.log(authors, "komponent")
     return (
         
         <section className="aboutUs" id="aboutUs">
-            <Logo />
 
+            <div className="container aboutUs-container">
+                <Logo />
             <article className="aboutUs-team">
 
                 {authors.map((item) => {
                     return (
                         <>
-                        <Link key={item.sys.id} href="/" passHref>
+                        {/* co wrzucić żeby się link wyświetlał poprawnie */}
+                        <Link 
+                            key={item.sys.id}
+                            href="/" 
+                            passHref
+                            >
                     <a>
-
-                        <Image loader={() => item.fields.photo.fields.file.url}
+                        {/* stylowanie w Image? */}
+                        <Image 
+                            loader={() => item.fields.photo.fields.file.url }
                             src={item.fields.photo.fields.file.url}
                             alt={item.fields.photo.fields.description}
                             width={100}
                             height={100}  
+                            className="photo"
                         />
 
                     </a>
@@ -43,21 +51,28 @@ console.log(authors, "komponent")
                 Jak pogodzić aktualną pracę z marzeniem o zmianie branży? 
                 Jak zbalansować czas poświęcony na odpoczynek, czas poświęcony na życie rodzinne, czas poświęcony na samorozwój? 
                 Jak poradzić sobie z - ostatnio popularnym określeniem - prokrastynacją?
-
+                </p>
+                <p>
                 Szukając odpowiedzi na te pytania znaleźliśmy drogę do...
                 wspólnego spotkania w barze i w takiej luźnej atmosferze, od słowa do słowa narodził się pomysł, 
                 który ma nas nawzajem motywować do regularnej pracy, do utrwalania już zdobytej wiedzy i dobrych praktyk 
                 oraz do rozwijania się w nowych technologiach.
-
+                </p>
+                <p>
                 Skąd nazwa - to esencja momentu, w którym się znaleźliśmy. 
                 Młodzi, aspirujący juniorzy, szukający swojego miejsca w IT - to nasz wspólny mianownik. 
                 To czym się różnimy i nasze indywidualne projekty znajdziecie w naszych indywidualnych sekcjach klikając w nasze miniaturki.
+                </p>
 
-                Pozdrawiamy</p>
-                <span>JuniorCoders</span>
+                <p>Pozdrawiamy</p>
+                <p className="aboutUs-signature">
+                    <span>JuniorCoders</span>
+                </p>
 
             </article>
+            </div>
         </section>
+        
     )
 }
 
