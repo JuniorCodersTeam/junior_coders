82c9365536e4e375cf83361345d98b5df05ec8b9
PawelPazderski <pawel.pazderski@op.pl>
2022-10-13 16:00:54 +0200

    404 styling

--- pages/404.jsx
+++ pages/404.jsx
@@ -1,8 +1,9 @@
 const Error = () => {
     return (
         <div className="nopage-container">
-            <h2 className="nopage-title">SORRY</h2>
-            <h3 className="nopage-title--info">page not found</h3>
+            <h2 className="nopage-title">PRZEPRASZAMY</h2>
+            <h3 className="nopage-title--info">strony nie odnaleziono</h3>
+            <h3 className="nopage-title--info">pracujemy nad tym</h3>
             <span className="nopage-number">404</span>
         </div>
     )
