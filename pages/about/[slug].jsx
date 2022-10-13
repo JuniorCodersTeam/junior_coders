c7cc9b5867ea58adf2e012169f40bd1021c9e784
grzeskid <szymanekgrzegorz@gmail.com>
2022-10-13 22:22:29 +0200

    rendering projects by imported ProjectCard

--- pages/about/[slug].jsx
+++ pages/about/[slug].jsx
@@ -1,6 +1,7 @@
 import {createClient} from "contentful";
 import Image from "next/image";
 import { Button } from "../../components/UI/Button";
+import { ProjectCard} from "../../components/Projects/ProjectCard"
 
 const client = createClient({
     space: process.env.CONTENTFUL_SPACE_ID,
@@ -62,30 +63,10 @@ const Author = ({items, foundProjects}) => {
                 </div>
 
                 <div  className="project-cards">
-                {foundProjects.map((project) => (
-                    <div key={project.fields.order} className="project-card">
-                    <Image
-                        alt={project.fields.title}
-                        src={`https:${project.fields.image.fields.file.url}`}
-                        width="384"
-                        height="288"
-                        className="blog-card--image"
-                        objectFit="cover"
-                        objectPosition="top"
-                    />
-                    <div className="title-bar">
-                        <h2>{project.fields.title}</h2>
-                    </div>
                     
-                    <p>{project.fields.description}</p>
-                    <div className="technologies">
-                        <h3>Technologie:</h3>
-                        <p>{project.fields.technologies}</p>
-                    </div>
-                    <div className="project-card--action">
-                        <Button current="projects" link={project.fields.slug} />
-                    </div>
-                </div> 
+                {foundProjects.map((project) => (
+                    
+                    <ProjectCard project={project} key={project.fields.order}/> 
                 
             ))}
 
