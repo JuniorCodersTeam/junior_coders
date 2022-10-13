feb10189a2685987c58d54a30aa2e047d20b855d
PawelPazderski <pawel.pazderski@op.pl>
2022-10-13 09:26:48 +0200

    theme & style to blog card

--- pages/blog/index.jsx
+++ pages/blog/index.jsx
@@ -22,6 +22,7 @@ export async function getStaticProps() {
 const blog = ({ posts }) => {
   return (
     <div className="blog-container">
+        <h2 className="projects-title">Blog</h2>
         <div className="blog">
         {posts.map((post) => (
           <BlogCard post={post} key={post.sys.id} />
