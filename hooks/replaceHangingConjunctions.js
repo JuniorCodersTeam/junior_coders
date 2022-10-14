199c9a0e9bc410cd91c42ab321492bd42aa0935b
PawelPazderski <pawel.pazderski@op.pl>
2022-10-14 10:58:57 +0200

    replace conjunction props rename

--- hooks/replaceHangingConjunctions.js
+++ hooks/replaceHangingConjunctions.js
@@ -1,5 +1,5 @@
-export function replaceHangingConjunctions(data) {
+export function replaceHangingConjunctions(text) {
     const regEx = /(\b)(a|i|o|u|w|z|A|I|O|U|W|Z)(\b\s)/g;
 
-    return data.replaceAll(regEx, "$2\u00a0")
+    return text.replaceAll(regEx, "$2\u00a0")
 }
