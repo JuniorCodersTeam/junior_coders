8f4d7e274ac1068ac84468dafe4a84391215552d
Kamil Kiliasiński <kamilkiliasinski@gmail.com>
2022-10-29 13:41:03 +0200

    fix nested routes

--- pages/projects/[slug]/index.jsx
+++ pages/projects/[slug]/index.jsx
@@ -1,90 +1,99 @@
-import {createClient} from "contentful";
+import { createClient } from "contentful";
 import { useRouter } from "next/router";
 import Link from "next/link";
 import { BsChevronRight } from "react-icons/bs";
 import { BsArrowRightShort } from "react-icons/bs";
 import Image from "next/image";
-import Projects from "..";
+import { useEffect } from "react";
 
 const client = createClient({
-    space: process.env.CONTENTFUL_SPACE_ID,
-    accessToken: process.env.CONTENTFUL_ACCESS_KEY,
+  space: process.env.CONTENTFUL_SPACE_ID,
+  accessToken: process.env.CONTENTFUL_ACCESS_KEY,
 });
 
-export const getStaticPaths = async() => {
-    const res = await client.getEntries({content_type: "projects"})
+export const getStaticPaths = async () => {
+  const res = await client.getEntries({
+    content_type: "projects",
+  });
 
-    const paths = res.items.map(item => {
-        return {
-            params: {
-                slug: item.fields.slug
-            }
-        }
-    })
+  const paths = res.items.map((item) => {
+    return {
+      params: {
+        slug: item.fields.slug,
+      },
+    };
+  });
 
-    return {paths, fallback: false}
-}
+  return { paths, fallback: false };
+};
 
-export const getStaticProps = async({params}) => {
-    const {items: project} = await client.getEntries({content_type: "projects", "fields.slug": params.slug})
-    
-    return {props: {project}, revalidate: 30}
-}
+export const getStaticProps = async ({ params }) => {
+  const { items: project } = await client.getEntries({
+    content_type: "projects",
+    "fields.slug": params.slug,
+  });
 
-const Project = ({project}) => {
-    const {
-        title,
-        slug,
-        image,
-        description,
-        technologies
-        } = project[0].fields;
+  return { props: { project }, revalidate: 30 };
+};
 
-    const router = useRouter()
-    const path = router.asPath.replaceAll("/", " ").split(" ").slice(1)
+const Project = ({ project }) => {
+  const router = useRouter();
 
-    return (
-        <>
-        <div className="projects-container">
-            <div className="path-links">
-                <Link href="/"><span className="path-link">home</span></Link>
-                <BsChevronRight className="back-icon" />
-                <span className="path-link" onClick={() => router.back()}>{path[0]}</span>
-                <BsChevronRight className="back-icon" />
-                <span className="path-link" onClick={() => router.reload()}>{path[1]}</span>
+  if (router.isFallback) {
+    return <div>Loading...</div>;
+  }
+  const { title, slug, image, description, technologies } = project[0].fields;
+
+  const path = router.asPath.replaceAll("/", " ").split(" ").slice(1);
+  return (
+    <>
+      <div className="projects-container">
+        <div className="path-links">
+          <Link href="/">
+            <span className="path-link">home</span>
+          </Link>
+          <BsChevronRight className="back-icon" />
+          <span className="path-link" onClick={() => router.back()}>
+            {path[0]}
+          </span>
+          <BsChevronRight className="back-icon" />
+          <span className="path-link" onClick={() => router.reload()}>
+            {path[1]}
+          </span>
+        </div>
+        <div className="project-detail-container">
+          <div className="project-detail-image">
+            <Image
+              alt={image.fields.title}
+              src={`https:${image.fields.file.url}`}
+              layout="fill"
+              objectFit="cover"
+            />
+            <h3 className="project-h3">{title}</h3>
+          </div>
+          <div className="project-detail-info">
+            <p className="project-p">{description}</p>
+            <h4 className="project-h4">Technologie:</h4>
+            <p className="project-p">{technologies}</p>
+            <div className="project-detail-buttons">
+              <Link href="/projects">
+                <span className="project-detail-link btn">
+                  Projekt Live
+                  <BsArrowRightShort className="arrow-icon" />
+                </span>
+              </Link>
+              <Link href="/projects">
+                <span className="project-detail-link btn">
+                  GitHub
+                  <BsArrowRightShort className="arrow-icon" />
+                </span>
+              </Link>
             </div>
-            <div className="project-detail-container">
-                <div className="project-detail-image">
-                    <Image
-                        alt={image.fields.title}
-                        src={`https:${image.fields.file.url}`}
-                        layout="fill"
-                        objectFit="cover"
-                    />
-                    <h3 className="project-h3">{title}</h3>
-                </div>
-                <div className="project-detail-info">
-                    <p className="project-p">{description}</p>
-                    <h4 className="project-h4">Technologie:</h4>
-                    <p className="project-p">{technologies}</p>
-                    <div className="project-detail-buttons">
-                        <Link href="/projects">
-                            <span className="project-detail-link btn">Projekt Live<BsArrowRightShort className="arrow-icon" /></span>
-                        </Link>
-                        <Link href="/projects">
-                            <span className="project-detail-link btn">GitHub<BsArrowRightShort className="arrow-icon" /></span>
-                        </Link>
-                    </div>
-                    
-                </div>
-            
-            </div>
-        </div>    
-        </>
-        
-    )
-}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Project;
-
-
